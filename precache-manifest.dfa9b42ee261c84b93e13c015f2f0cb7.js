9469a555de1dd5bc14167b8fc8b5f67d554636c4
pandurijal <pandurijal96@gmail.com>
2020-02-03 23:37:52 +0700

    Updates

---
+++ precache-manifest.dfa9b42ee261c84b93e13c015f2f0cb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4799fcb03746f0febe34c922e5e998",
+    "url": "/advotics-dashboard/index.html"
+  },
+  {
+    "revision": "45638d069ae3e6927f62",
+    "url": "/advotics-dashboard/static/css/2.4118c0c4.chunk.css"
+  },
+  {
+    "revision": "9036c195427e710767ed",
+    "url": "/advotics-dashboard/static/css/main.1765078c.chunk.css"
+  },
+  {
+    "revision": "45638d069ae3e6927f62",
+    "url": "/advotics-dashboard/static/js/2.6160e27c.chunk.js"
+  },
+  {
+    "revision": "977b85ae92e0fb68aebdece70c5a2518",
+    "url": "/advotics-dashboard/static/js/2.6160e27c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9036c195427e710767ed",
+    "url": "/advotics-dashboard/static/js/main.ca3424d4.chunk.js"
+  },
+  {
+    "revision": "2f96f7407051dfac98a5",
+    "url": "/advotics-dashboard/static/js/runtime-main.448c4b42.js"
+  }
+]);
